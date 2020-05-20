46a60ca363e80c1d6865f843c32289313082d8b9
Mauricio Garcia <fidel.garcia@globant.com>
2020-05-20 02:12:14 -0500

    fix update with particles and new logo

---
+++ src/index.js
@@ -0,0 +1,19 @@
+
+particlesJS.load('particles-js', './particles.json', function () {
+  console.log('callback - particles-js config loaded');
+});
+
+const move = () => {
+  const elem = document.getElementById('myBar');
+  let width = 1;
+  const id = setInterval(() => {
+    if (width >= 100) {
+      width = 0;
+    } else {
+      width++;
+      elem.style.width = width + '%';
+    }
+  }, 30);
+};
+
+move();
